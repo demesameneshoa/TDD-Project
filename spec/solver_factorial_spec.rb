fbd16444f42c0d1a7b52d2ff2c45f8ee1c7bcc4b
demesameneshoa <demesameneshoa@gmail.com>
2023-11-16 22:00:53 +0300

    Resolved Offences on Solver Factorial Spec

--- spec/solver_factorial_spec.rb
+++ spec/solver_factorial_spec.rb
@@ -1,22 +1,22 @@
 require_relative '../solver'
 
 RSpec.describe Solver do
-    describe '#factorial' do
-      it 'returns 1 for input 0' do
-        expect(Solver.factorial(0)).to eq(1)
-      end
-  
-      it 'returns 1 for input 1' do
-        expect(Solver.factorial(1)).to eq(1)
-      end
-  
-      it 'returns the correct factorial for positive integers' do
-        expect(Solver.factorial(5)).to eq(120)
-        # Add more test cases as needed
-      end
-  
-      it 'raises an ArgumentError for negative integers' do
-        expect { Solver.factorial(-1) }.to raise_error(ArgumentError)
-      end
+  describe '#factorial' do
+    it 'returns 1 for input 0' do
+      expect(Solver.factorial(0)).to eq(1)
+    end
+
+    it 'returns 1 for input 1' do
+      expect(Solver.factorial(1)).to eq(1)
+    end
+
+    it 'returns the correct factorial for positive integers' do
+      expect(Solver.factorial(5)).to eq(120)
+      # Add more test cases as needed
+    end
+
+    it 'raises an ArgumentError for negative integers' do
+      expect { Solver.factorial(-1) }.to raise_error(ArgumentError)
     end
   end
+end
