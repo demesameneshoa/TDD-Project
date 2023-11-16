2bc13c2142d17decf1c4bfc50f22c50751e237a7
gdvl777 <gdvl777@users.noreply.github.com>
2023-11-16 14:47:12 -0500

    Update solver_reverse_spec.rb

--- spec/solver_reverse_spec.rb
+++ spec/solver_reverse_spec.rb
@@ -5,22 +5,22 @@ require_relative '../solver'
 describe '#reverse' do
   context 'when a string is provided' do
     it 'reverses the string' do
-      expect(reverse('hello')).to eq('olleh')
-      expect(reverse('world')).to eq('dlrow')
+      expect(Solver.reverse('hello')).to eq('olleh')
+      expect(Solver.reverse('world')).to eq('dlrow')
     end
   end
 
   context 'when an array is provided' do
     it 'reverses the array' do
-      expect(reverse([1, 2, 3])).to eq([3, 2, 1])
-      expect(reverse(%w[a b c])).to eq(%w[c b a])
+      expect(Solver.reverse([1, 2, 3])).to eq([3, 2, 1])
+      expect(Solver.reverse(%w[a b c])).to eq(%w[c b a])
     end
   end
 
   context 'when other data types are provided' do
     it 'raises an error' do
-      expect { reverse(123) }.to raise_error(ArgumentError)
-      expect { reverse(nil) }.to raise_error(ArgumentError)
+      expect { Solver.reverse(123) }.to raise_error(ArgumentError)
+      expect { Solver.reverse(nil) }.to raise_error(ArgumentError)
     end
   end
 end
