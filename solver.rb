d65dbe6f0abf72c2ae6a0caa04517e1e8dd822b3
demesameneshoa <demesameneshoa@gmail.com>
2023-11-16 22:44:26 +0300

    Added factorial method in the solver class

--- solver.rb
+++ solver.rb
@@ -3,4 +3,12 @@ class Solver
   def reverse(word)
     word.reverse
   end
+
+  def self.factorial(num)
+    raise ArgumentError, 'Input must be a non-negative integer' unless num.is_a?(Integer) && num >= 0
+
+    return 1 if num.zero?
+
+    (1..n).reduce(:*)
+  end
 end
